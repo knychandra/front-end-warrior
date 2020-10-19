0da74277c6a5fb478e61c74ead864a1a77cb58c8
Kenny Chandra <43095847+knychandra@users.noreply.github.com>
2020-10-19 10:50:38 +0700

    upload final result

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,5 @@
+// const invalidEmail = document.querySelector('.invalidEmail');
+// const submit = document.querySelector('.submit');
+// const email = document.querySelector('section.email form input');
+// const errorIcon = document.querySelector('.error-icon')
+
